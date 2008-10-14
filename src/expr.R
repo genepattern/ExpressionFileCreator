bc1b3fa21f0c095e7207b27e16e4f6f8d7b1e732
jgould <jgould@47fccdf2-e235-0410-87e5-63258e95d6e5>
2008-10-14 12:47:53 +0000

    check custom cdf file
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/ExpressionFileCreator@15146 47fccdf2-e235-0410-87e5-63258e95d6e5

--- src/expr.R
+++ src/expr.R
@@ -183,6 +183,10 @@ create.expression.file <- function(input.file.name, output.file.name, method, qu
 		}
 		library(makecdfenv)
 		mycdfenv <<- make.cdf.env(filename=basename(cdf.file), cdf.path=dirname(cdf.file), verbose=F)
+		index <- grep(chip, basename(cdf.file), ignore.case=T)
+		if(length(index) == 0) {
+		   exit("The custom cdf file provided does not appear to be from the ", chip, " chip.")	
+		}
 	}
 	
 	compressed <- is.compressed(cel.file.names)

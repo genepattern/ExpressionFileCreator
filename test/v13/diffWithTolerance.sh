08a783bfd210868c2636ba900122c63a3597f158
eby <eby@47fccdf2-e235-0410-87e5-63258e95d6e5>
2015-03-31 12:30:47 +0000

    Fixed an issue with the diffWithTolerance comparisons.
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/ExpressionFileCreator@35077 47fccdf2-e235-0410-87e5-63258e95d6e5

--- test/v13/diffWithTolerance.sh
+++ test/v13/diffWithTolerance.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 diffdir=`mktemp -d diffdir.XXXXXX`
+mkdir $diffdir/in1
+mkdir $diffdir/in2
 base1=`basename $1`
 base2=`basename $2`
 
-ln -s $1 $diffdir/$base1
-ln -s $2 $diffdir/$base2
+ln -s $1 $diffdir/in1/$base1
+ln -s $2 $diffdir/in2/$base2
 
 cd $diffdir
-/Library/Frameworks/R.framework/Versions/2.15/Resources/bin/Rscript --no-save --quiet --slave --no-restore ~/Documents/workspace/GenePattern/modules/DiffDatasets/src/run_diff_datasets.R ~/Documents/workspace/GenePattern/modules/DiffDatasets/src/ /Applications/GenePatternServer /Applications/GenePatternServer/patches/ --first.input.file="$base1" --second.input.file="$base2" --round.method=round --round.digits=6
+/Library/Frameworks/R.framework/Versions/2.15/Resources/bin/Rscript --no-save --quiet --slave --no-restore ~/Documents/workspace/GenePattern/modules/DiffDatasets/src/run_diff_datasets.R ~/Documents/workspace/GenePattern/modules/DiffDatasets/src/ /Applications/GenePatternServer /Applications/GenePatternServer/patches/ --first.input.file="in1/$base1" --second.input.file="in2/$base2" --round.method=round --round.digits=6
+status=$?
 cd ..
 rm -rf $diffdir
+exit $status

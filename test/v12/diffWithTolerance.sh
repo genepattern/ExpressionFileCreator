f13465094e65631f8671dd4a33e33e1c92ab4242
eby <eby@47fccdf2-e235-0410-87e5-63258e95d6e5>
2015-04-14 15:31:11 +0000

    Fixing a bug in diffWithTolerance.sh
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/ExpressionFileCreator@35094 47fccdf2-e235-0410-87e5-63258e95d6e5

--- test/v12/diffWithTolerance.sh
+++ test/v12/diffWithTolerance.sh
@@ -5,8 +5,8 @@ mkdir $diffdir/in2
 base1=`basename $1`
 base2=`basename $2`
 
-ln -s $1 $diffdir/in1/$base1
-ln -s $2 $diffdir/in2/$base2
+ln $1 $diffdir/in1/$base1
+ln $2 $diffdir/in2/$base2
 
 cd $diffdir
 /Library/Frameworks/R.framework/Versions/2.15/Resources/bin/Rscript --no-save --quiet --slave --no-restore ~/Documents/workspace/GenePattern/modules/DiffDatasets/src/run_diff_datasets.R ~/Documents/workspace/GenePattern/modules/DiffDatasets/src/ /Applications/GenePatternServer /Applications/GenePatternServer/patches/ --first.input.file="in1/$base1" --second.input.file="in2/$base2" --round.method=round --round.digits=6

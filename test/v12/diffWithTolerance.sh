4aa802aa4c75b749ca208321df0d6b85c0a565d7
eby <eby@47fccdf2-e235-0410-87e5-63258e95d6e5>
2015-04-21 20:03:20 +0000

    Updated diffWithTollerance script to use more gpunit-friendly entry point.  Still not totally portable but much closer.
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/ExpressionFileCreator@35104 47fccdf2-e235-0410-87e5-63258e95d6e5

--- test/v12/diffWithTolerance.sh
+++ test/v12/diffWithTolerance.sh
@@ -9,7 +9,7 @@ ln $1 $diffdir/in1/$base1
 ln $2 $diffdir/in2/$base2
 
 cd $diffdir
-/Library/Frameworks/R.framework/Versions/2.15/Resources/bin/Rscript --no-save --quiet --slave --no-restore ~/Documents/workspace/GenePattern/modules/DiffDatasets/src/run_diff_datasets.R ~/Documents/workspace/GenePattern/modules/DiffDatasets/src/ /Applications/GenePatternServer /Applications/GenePatternServer/patches/ --first.input.file="in1/$base1" --second.input.file="in2/$base2" --round.method=round --round.digits=6
+Rscript --no-save --quiet --slave --no-restore ~/Documents/workspace/GenePattern/modules/DiffDatasets/src/run_diff_datasets_gpunit.R --first.input.file="in1/$base1" --second.input.file="in2/$base2" --round.method=round --round.digits=6
 status=$?
 cd ..
 rm -rf $diffdir

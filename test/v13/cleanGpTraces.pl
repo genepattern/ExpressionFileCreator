fe33b1b886311b9638e6c77c898fef24cd3cd569
eby <eby@47fccdf2-e235-0410-87e5-63258e95d6e5>
2015-10-09 23:33:51 +0000

    Minor gpunit changes to update to a more modern version along with testing for the new package installer.
    git-svn-id: https://svn.broadinstitute.org/gp2/trunk/modules/ExpressionFileCreator@35321 47fccdf2-e235-0410-87e5-63258e95d6e5

--- test/v13/cleanGpTraces.pl
+++ test/v13/cleanGpTraces.pl
@@ -2,6 +2,9 @@ use strict;
 use warnings;
 
 while (my $line = <>) {
+    # The following have changed over time with various GP releases.
+    next if ($line =~ /Process exited with an error/);  # Local server executor appends this as final line
+    next if ($line =~ /Process exited with status code:/);  # Local server executor appends this as final line
     next if ($line =~ /[Ee]xited with (status)|(exit) code/);  # Local server executor appends this as final line; exact text varies by GP version.
 
     $line =~ s/\[[\d]{2}:[\d]{2}:[\d]{2}\]/\[timestamp\]/g;
--- test/v13/diffWithTolerance.sh
+++ test/v13/diffWithTolerance.sh
@@ -9,7 +9,7 @@ ln $1 $diffdir/in1/$base1
 ln $2 $diffdir/in2/$base2
 
 cd $diffdir
-Rscript --no-save --quiet --slave --no-restore ~/Documents/workspace/GenePattern/modules/DiffDatasets/src/run_diff_datasets_gpunit.R --first.input.file="in1/$base1" --second.input.file="in2/$base2" --round.method=round --round.digits=6
+~/.genepattern/scripts/r215_wrapper.sh --no-save --quiet --slave --no-restore ~/Documents/workspace/GenePattern/modules/DiffDatasets/src/run_diff_datasets_gpunit.R --first.input.file="in1/$base1" --second.input.file="in2/$base2" --round.method=round --round.digits=6
 status=$?
 cd ..
 rm -rf $diffdir
